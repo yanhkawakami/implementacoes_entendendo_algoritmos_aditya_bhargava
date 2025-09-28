f5706d61710e66c20a0a28bd7917c58a00dfa469
Yan Hideki Kawakami <yanhkawakami@gmail.com>
2025-09-28 20:08:50 -0300

    add: adição dos algoritmos do capítulo 7

---
+++ capitulo7/busca_largura.py
@@ -0,0 +1,25 @@
+# Busca em Largura
+# Tempo de execução: O(V + E) onde V é o número de vértices e E é o número de arestas no grafo.
+from collections import deque
+import grafos
+
+
+def busca_largura():
+    fila_de_pesquisa = deque()
+    fila_de_pesquisa += grafos.grafo["voce"]
+    verificadas = []
+    while fila_de_pesquisa:
+        pessoa = fila_de_pesquisa.popleft()
+        if not pessoa in verificadas:
+            if pessoa_e_vendedor(pessoa):
+                print(pessoa + " é um vendedor de manga!")
+                return True
+            else:
+                fila_de_pesquisa += grafos.grafo[pessoa]
+                verificadas.append(pessoa)
+    return False
+
+def pessoa_e_vendedor(nome):
+    return nome[-1] == "m"
+
+busca_largura()
